92628a5fe12eca11a31d5c6217ae8b23985950e0
mark <mark@mdmorris.co.uk>
2017-11-03 11:06:26 +0000

    changed sttings to have media file outside of source

--- mdmorris_blog/settings.py
+++ mdmorris_blog/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR 
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../media')) 
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 
